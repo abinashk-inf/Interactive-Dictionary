4bf61b612e43a49ef2625249c7f200589f957028
abinashk-inf <abinashkr94@gmail.com>
2017-10-20 17:11:23 +0530

    Handle error if word is not found

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import json
 global data_file
 
 def definition(word):
-    print(data_file[word])
-    
+    try:
+        print(data_file[word])
+    except:
+        print("Word not found")
 
 if __name__=='__main__':
     with open('data.json') as fh:
