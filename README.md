f6ded8884d7e014c2e171882ccd903d50693a5be
abinashk-inf <abinashkr94@gmail.com>
2017-10-21 16:11:47 +0530

    Add suggestions for non matching words

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Interactive-Dictionary
-This project is being carried out to comprehend various python concepts. The Readme.md will be updated once a new feature is added.  
+This project is being carried out to comprehend useful libraries and python concepts. Current features:
+    1. Case insensitive
+    2. Suggest a list of four words if current word is not found. The list is ordered based on similarity.
+
+
+The Readme will be updated if a new feature is added.  
--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 import json
+import difflib
+
+
 global data_file
 
 def definition(word):
-    try:
+    word=word.lower()
+    if word in data_file:
         print(data_file[word])
-    except:
-        print("Word not found")
+    else:
+        
+        related=difflib.get_close_matches(word,data_file.keys(),n=4,cutoff=0.6)
+        if len(related) > 0:
+            print("The Entry did not match any word. Are you loooking for any of these?")
+            print(related)
+        else:
+            print("Word not found")
 
 if __name__=='__main__':
     with open('data.json') as fh:
